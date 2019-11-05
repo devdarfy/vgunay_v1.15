4661522912acfdba9f148131e6d3a168c2d854a8
VolkanGünay <vgunayiletisim@gmail.com>
2019-11-05 08:16:10 +0300

    v1.15 update!
    Tüm tema renkleri değiştirildi.
    ReklamVer sayfası kaldırıldı.
    Footer içerisinde ki sosyal medya kısmı değiştirildi.
    404 Not Found Sayfası eklendi.

---
+++ 404.php
@@ -0,0 +1,25 @@
+<?php
+
+include'header.php'; /*  Yapılcak */
+?>
+
+
+<center style="font-size:50px;margin-top:300px;font-family:monospace;color:#333;">404 Not Found! <br>Aradığınız sayfa bulunamadı.</center><br><br>
+
+<a href="http://vgunay.com/" style="text-decoration:none;">
+<div style="border:5px solid #4CAF50;
+    width:160px;
+    font-size:18px;
+    text-align:center;
+    border-radius:30px;
+    padding:10px;
+    position:relative;
+    color:black;font-family:monospace;margin-left:850px;">Anasayfa'ya Dön</div></a>
+    
+   <br>
+   <center style="font-size:30px;font-family:monospace;color:#333;">Yeni 404 Sayfası yapım aşamasındadır.</center>
+
+<?php
+
+include'footer.php'; /*  Yapılcak */
+?>
